45deba538185d08793a0373655ca97d885f9da68
asloofdev <asloofdev@gmail.com>
2025-06-19 00:29:20 +0530

    frontend intalled and intiated

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+function App() {
+
+  return (
+    <>
+     <div>
+      <p className='text4-xl text-blue-600'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio deserunt, nihil consectetur ipsa eius adipisci reiciendis similique. Sunt architecto obcaecati voluptatum repellat, accusamus ullam necessitatibus inventore ipsum iusto optio.
+      </p>
+     </div>
+    </>
+  )
+}
+
+export default App
