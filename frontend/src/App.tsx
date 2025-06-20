926135ea25d10de121a97991f5f0a6815184051f
asloofdev <asloofdev@gmail.com>
2025-06-20 22:41:19 +0530

    NavBar and Styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,25 @@
+import{
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom"
+import Layout from "./Layout"
 
 function App() {
 
   return (
-    <>
-     <div>
-      <p className='text4-xl text-blue-600'>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio deserunt, nihil consectetur ipsa eius adipisci reiciendis similique. Sunt architecto obcaecati voluptatum repellat, accusamus ullam necessitatibus inventore ipsum iusto optio.
-      </p>
-     </div>
-    </>
+    <Router>
+      <Routes>
+        <Route path = "/" element={
+          <Layout>
+            
+          </Layout>
+        }>
+
+        </Route>
+      </Routes>
+    </Router>
   )
 }
 
---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom"
+
+const Header = () =>{
+    return(
+        <div className="bg-blue-500 py-6" >
+            <div className="container flex justify-between">
+                <span className="text-3xl text-white font-bold tracking-tight">
+                    <Link to= "/">KingSha</Link>
+                </span>
+                <span className="flex space 2 ">
+                    <Link to="/sign-in"
+                    className="flex bg-white items-center text-blue-500 px-3 font-bold hover:bg-gray-500">
+                    Sign In
+                    </Link>
+                </span>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
