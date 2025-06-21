35793d71978e57b0bf18259402e25cbbe9650b90
asloofdev <asloofdev@gmail.com>
2025-06-21 09:58:20 +0530

    Footer

--- frontend/src/Layout.tsx
+++ frontend/src/Layout.tsx
@@ -1,9 +1,11 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header"
 
 const Layout = () =>{
     return(
         <div className="flex flex-col min-h-screen">
             <Header/>
+            <Footer/>
 
         </div>
     )
