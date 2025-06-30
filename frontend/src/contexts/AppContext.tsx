d6a9e79a79e11b6890da7a9fb247c6b9ea82f4f9
asloofdev <asloofdev@gmail.com>
2025-06-30 19:55:55 +0530

    f - understand the appcontext and type with that tost component created

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -1,14 +1,35 @@
+import React, { useContext } from "react";
+
 type ToastMessage = {
     message:string;
     type:"SUCCESS" | "ERROR"
 }
 
-type AppContext = {
+
+
+type AppContextType = {
     showToast:(toastMessage: ToastMessage)=>void
 }
 
-const AppContext = React.createContext<AppContext | undefined>(undefined)
+const AppContext = React.createContext<AppContextType | undefined>(undefined)
 
-export const AppContextProvider = ()=>{
-    
+export const AppContextProvider = ({children}:{children:React.ReactNode})=>{
+    return(
+        <AppContext.Provider value={
+            {showToast:(toslike)=>{
+                console.log(toslike);
+                
+            }}
+        }>
+            {children}
+        </AppContext.Provider> 
+    )
+}
+
+export const useAppContext = ()=>{
+   const context = useContext(AppContext);
+    if (context === undefined) {
+        throw new Error('useAppContext must be used within an AppContextProvider');
+    }
+    return context;
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import './index.css'
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AppContextProvider } from './contexts/AppContext.tsx'
 
 const queryClient = new QueryClient({
   defaultOptions:{
@@ -15,7 +16,9 @@ const queryClient = new QueryClient({
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <AppContextProvider>
+          <App />
+      </AppContextProvider>
     </QueryClientProvider>
   </StrictMode>,
 )
