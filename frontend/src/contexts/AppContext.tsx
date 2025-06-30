00c2f10d306a32744c3db0401a80fff5ee5edd41
asloofdev <asloofdev@gmail.com>
2025-06-30 17:47:58 +0530

    f - creating context for toast

---
+++ frontend/src/contexts/AppContext.tsx
@@ -0,0 +1,14 @@
+type ToastMessage = {
+    message:string;
+    type:"SUCCESS" | "ERROR"
+}
+
+type AppContext = {
+    showToast:(toastMessage: ToastMessage)=>void
+}
+
+const AppContext = React.createContext<AppContext | undefined>(undefined)
+
+export const AppContextProvider = ()=>{
+    
+}
