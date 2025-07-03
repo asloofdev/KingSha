953c4563d8bd8dac67eae1b3934818acac8493f6
asloofdev <asloofdev@gmail.com>
2025-07-03 20:49:40 +0530

    b - finished auth.ts middle ware

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,7 +1,15 @@
 import { NextFunction,Request,Response} from "express";
-import jwt from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken";
 
-const verfyToken = (req:Request,res:Response,nextFunction:NextFunction) =>{
+declare global{
+    namespace Express{
+        interface Request{
+            userId:string;
+        }
+    }
+}
+
+const verfyToken = (req:Request,res:Response,next:NextFunction) =>{
     const token = req.cookies["auth_token"];
 
     if(!token){
@@ -10,8 +18,12 @@ const verfyToken = (req:Request,res:Response,nextFunction:NextFunction) =>{
 
     try{
         const decode = jwt.verify(token,process.env.JWT_SECRETE_KEY as string)
+        req.userId = (decode as JwtPayload).userId
+        next()
     }
     catch(error ){
         res.status(401).json({message:'unauthorized'})
     }
 } 
+
+export default verfyToken;
