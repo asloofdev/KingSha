b9bed1519f997a3123de7c22f03c489bc7811293
asloofdev <asloofdev@gmail.com>
2025-07-05 22:34:12 +0530

    f - sign out button logic

---
+++ frontend/src/components/SignOut.tsx
@@ -0,0 +1,9 @@
+const SignOutButton = () =>{
+    return(
+        <button className="text-blue-500 px-3 font-bold bg-white hover:bg-gray-700">
+            Sign Out
+        </button>
+    )
+}
+
+export default SignOutButton
