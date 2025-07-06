49688b9a808cb7eac8f80090b2375e6d23946b83
asloofdev <asloofdev@gmail.com>
2025-07-06 09:47:39 +0530

    f - logout is complete

--- frontend/src/components/SignOut.tsx
+++ frontend/src/components/SignOut.tsx
@@ -1,6 +1,28 @@
+import { useMutation } from "@tanstack/react-query"
+import * as apiClient from "../api-client"
+import { useAppContext } from "../contexts/AppContext"
+
 const SignOutButton = () =>{
+
+    const {showToast} = useAppContext();
+
+    const mutaton = useMutation({
+        mutationFn:apiClient.logout,
+        onSuccess:()=>{
+            showToast({message:"Log Out",type:"SUCCESS"
+            })
+        },
+        onError:(error:Error)=>{
+            showToast({message:error.message,type:"ERROR"})
+        }
+    })
+
+    const handleClick =()=>{
+        mutaton.mutate()
+    }
+
     return(
-        <button className="text-blue-500 px-3 font-bold bg-white hover:bg-gray-700">
+        <button onClick={handleClick} className="text-blue-500 px-3 font-bold bg-white hover:bg-gray-700">
             Sign Out
         </button>
     )
