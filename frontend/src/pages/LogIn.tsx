07d145955c27359f2269d6396258faead4d1bd82
asloofdev <asloofdev@gmail.com>
2025-07-07 19:38:18 +0530

    f - remove the extra line

--- frontend/src/pages/LogIn.tsx
+++ frontend/src/pages/LogIn.tsx
@@ -2,8 +2,7 @@ import { useMutation,  useQueryClient } from "@tanstack/react-query";
 import {useForm} from "react-hook-form"
 import * as apiClient from "../api-client"
 import { useAppContext } from "../contexts/AppContext";
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { useNavigate,Link} from "react-router-dom";
 
 export type LogInFormData ={
     email:string;
