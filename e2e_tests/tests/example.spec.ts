c168bfd447b2fd73d7726603f66150e7d2b349e4
asloofdev <asloofdev@gmail.com>
2025-07-12 22:59:18 +0530

    e - registration testcase is completed

--- e2e_tests/tests/example.spec.ts
+++ e2e_tests/tests/example.spec.ts
@@ -19,13 +19,15 @@ test('should not allow the user to log in ', async ({ page }) => {
 });
 
 test('should allow the user to register ',async ({page})=>{
+
+  const testEmail = `test_register_${Math.floor(Math.random()*90000)+10000}@test.com`
   await page.goto(UI_URL)
   await page.getByRole("link",{name:"Sign In"}).click()
   await expect(page.getByRole("heading",{name:"Create Account"})).toBeVisible()
 
   await page.locator("[name=firstName]").fill("test2")
   await page.locator("[name=lastName]").fill("fill2")
-  await page.locator("[name=email]").fill("test2@fill.com")
+  await page.locator("[name=email]").fill(testEmail)
   await page.locator("[name=password]").fill("test@1")
   await page.locator("[name=confirmPassword]").fill("test@1")
 
