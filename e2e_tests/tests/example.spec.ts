08743ed6592fc41a2daa955d5ee52c7e1caac849
asloofdev <asloofdev@gmail.com>
2025-07-12 22:52:06 +0530

    e - test working successfull after adding the roles

--- e2e_tests/tests/example.spec.ts
+++ e2e_tests/tests/example.spec.ts
@@ -23,15 +23,18 @@ test('should allow the user to register ',async ({page})=>{
   await page.getByRole("link",{name:"Sign In"}).click()
   await expect(page.getByRole("heading",{name:"Create Account"})).toBeVisible()
 
-  await page.locator("[name=firstName]").fill("test1")
-  await page.locator("[name=lastName]").fill("fill1")
-  await page.locator("[name=email]").fill("test1@fill.com")
+  await page.locator("[name=firstName]").fill("test2")
+  await page.locator("[name=lastName]").fill("fill2")
+  await page.locator("[name=email]").fill("test2@fill.com")
   await page.locator("[name=password]").fill("test@1")
   await page.locator("[name=confirmPassword]").fill("test@1")
 
   await page.getByRole("button",{name:"Create Account"}).click()
 
   await expect(page.getByText("Registration Success")).toBeVisible()
+  await expect(page.getByRole("link",{name:"My Bookings"})).toBeVisible()
+  await expect(page.getByRole("link",{name:"My Hotels"})).toBeVisible()
+  await expect(page.getByRole("button",{name:"Sign Out"})).toBeVisible()
 })
 
 test('get started link', async ({ page }) => {
