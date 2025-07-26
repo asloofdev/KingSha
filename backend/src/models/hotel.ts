4cddc764be233bd6074b8606de7f51dd52fec87e
asloofdev <asloofdev@gmail.com>
2025-07-26 22:07:38 +0530

    b - mongoose fields added

--- backend/src/models/hotel.ts
+++ backend/src/models/hotel.ts
@@ -15,5 +15,14 @@ export type HotelType = {
 }
 
 const hotelSchema = new mongoose.Schema<HotelType>({
-    
+    userId:{type:String, required:true},
+    name:{type:String, required:true},
+    city:{type:String, required:true},
+    country :{type:String, required:true},
+    description:{type:String, required:true},
+    type:{type:String, required:true},
+    adultCount:{type:Number, required:true},
+    childCount:{type:Number, required:true},
+    facilities:[{type:String, required:true}],
+    pricePerNight:{type:Number, required:true},
 })
