2d36366fcdece15e8ae6aed031e6ff9569210a0e
asloofdev <asloofdev@gmail.com>
2025-07-26 22:02:19 +0530

    b - hote model type define

---
+++ backend/src/models/hotel.ts
@@ -0,0 +1,15 @@
+export type HotelType = {
+    _id:string;
+    userId:string;
+    name:string;
+    city:string;
+    country :string;
+    description:string;
+    type:string;
+    adultCount:number;
+    childCount:number;
+    facilities:string[];
+    pricePerNight:number;
+
+
+}
