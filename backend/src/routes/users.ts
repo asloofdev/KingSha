d0816a5a35bdd28c897a1ebf341a69738b9412e0
asloofdev <asloofdev@gmail.com>
2025-07-28 23:10:25 +0530

    b - eeror fixed

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -9,14 +9,7 @@ const router = express.Router();
 router.post("/register",
   [
     check("firstName","First Name is required").isString(),
-    check("lastName","
-      
-      
-      
-      
-      
-      
-      last Name is required").isString(),
+    check("lastName","Fast Name is required").isString(),
     check("email","email is required").isEmail(),
     check("password"," password with 6 or more character required").isLength({min:6})
   ]
