36bd08a02eb677081f451109fc590128f193cca0
asloofdev <asloofdev@gmail.com>
2025-07-28 23:09:18 +0530

    b - Gramm

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -8,7 +8,7 @@ const router = express.Router();
 
 router.post("/register",
   [
-    check("firstName","first Name is required").isString(),
+    check("firstName","First Name is required").isString(),
     check("lastName","last Name is required").isString(),
     check("email","email is required").isEmail(),
     check("password"," password with 6 or more character required").isLength({min:6})
